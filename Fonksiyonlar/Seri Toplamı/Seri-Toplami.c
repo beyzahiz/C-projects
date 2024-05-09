bc90fc87f10fe8d587f4c03e3cadfb6316fd6b74
beyzahiz <130295983+beyzahiz@users.noreply.github.com>
2024-05-09 23:05:04 +0300

    Create Seri-Toplami.c

---
+++ Fonksiyonlar/Seri Toplamı/Seri-Toplami.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <math.h>
+
+int seritoplami(int N)
+{
+    int i;
+    int toplam=0;
+    for (i=1; i<=N; i++)
+    {
+        toplam=toplam+(pow(i,2));
+    }
+    return toplam;
+}
+
+int main() {
+
+    int n;
+    printf("N degerini giriniz: ");
+    scanf("%d",&n);
+    printf("Serinin Toplami: %d\n",seritoplami(n));
+    
+    return 0;
+}
